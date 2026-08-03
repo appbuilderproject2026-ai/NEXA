ef6bb13e4b3fb850bfbecda580f0688538e0ea91
appbuilderproject2026-ai <appbuilderproject2026@gmail.com>
2026-08-03 11:51:39 +0900

    Update home.js

--- home.js
+++ home.js
@@ -1,1 +1,44 @@
-console.log("home.js 読み込み成功");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/12.17.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDcR8V986QrxfLRynWZHC_05xIHX7fvBt0",
+  authDomain: "nexa-70fc4.firebaseapp.com",
+  projectId: "nexa-70fc4",
+  storageBucket: "nexa-70fc4.firebasestorage.app",
+  messagingSenderId: "905166333298",
+  appId: "1:905166333298:web:d55fdc338a53fdf236a9eb",
+  measurementId: "G-F598Y65CZC"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const chatList = document.getElementById("chatList");
+
+async function loadUsers() {
+
+  chatList.innerHTML = "";
+
+  const querySnapshot = await getDocs(collection(db, "users"));
+
+  querySnapshot.forEach((doc) => {
+
+    const user = doc.data();
+
+    chatList.innerHTML += `
+      <div class="chat">
+        <h3>${user.email}</h3>
+        <p>まだメッセージはありません</p>
+      </div>
+    `;
+
+  });
+
+}
+
+loadUsers();
