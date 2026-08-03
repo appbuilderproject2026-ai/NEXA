6e058bc931678529ebf2639d6326b5a6dcd1776d
appbuilderproject2026-ai <appbuilderproject2026@gmail.com>
2026-08-03 13:50:03 +0900

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,1 +1,9 @@
-console.log("chat.js 読み込み成功");
+const params = new URLSearchParams(window.location.search);
+
+const userEmail = params.get("user");
+
+const chatTitle = document.getElementById("chatTitle");
+
+if (userEmail) {
+  chatTitle.textContent = userEmail;
+}
